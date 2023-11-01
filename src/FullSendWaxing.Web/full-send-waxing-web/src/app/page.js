ad7d11fdd643dd6666f3fd574c18129757fe60c6
davenport1 <61665447+davenport1@users.noreply.github.com>
2023-11-01 09:48:11 -0700

    Mission component, navbar component, styling

--- src/FullSendWaxing.Web/full-send-waxing-web/src/app/page.js
+++ src/FullSendWaxing.Web/full-send-waxing-web/src/app/page.js
@@ -2,75 +2,47 @@
 import Image from 'next/image'
 import React  from 'react';
 import Header from '../components/Header/Header.js';
-import {
-  AiFillInstagram,
-  AiFillLinkedin,
-  AiFillYoutube,
-} from "react-icons/ai";
-import { BsFillMoonStarsFill } from "react-icons/bs";
+// import {
+//   AiFillInstagram,
+//   AiFillLinkedin,
+//   AiFillYoutube,
+// } from "react-icons/ai";
+// import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
 
-import FswLogoWhite from "../../public/logolight.png";
-import FswNegBlack from "../../public/logodark.png";
-import FswNegColor from "../../public/logocolordark.png";
-import FswColor from "../../public/logocolorlight.png";
-import CloseWork1 from "../../public/closework1.jpg";
-import CloseWork2 from "../../public/closework2.jpg";
-import ProductBest from "../../public/productbest.jpg";
-import ProductCloseUp from "../../public/productclose.jpg";
-import Products1 from "../../public/products1.jpg";
-import FarShotBest from "../../public/farshotbest.jpg";
-import CloseBest from "../../public/closebest.jpg";
-import EdgeWork1 from "../../public/edgework1.jpg";
-import FarShot1 from "../../public/farshot1.jpg";
-import FightClimateChange from "../../public/Fight-climate-change-art.jpg";
-import KeepTahoeBlue from "../../public/keeptahoeblue.png";
-import Kirkwood from "../../public/logo-kirkwood.png";
-import Mammoth from "../../public/mammothlogo.jpg";
-import Pow from "../../public/POW.png";
-import products1 from "../../public/products1.jpg";
-import Purl from "../../public/purllogo.png";
+// import FswLogoWhite from "../../public/logolight.png";
+// import FswNegBlack from "../../public/logodark.png";
+// import FswNegColor from "../../public/logocolordark.png";
+// import FswColor from "../../public/logocolorlight.png";
+// import CloseWork1 from "../../public/closework1.jpg";
+// import CloseWork2 from "../../public/closework2.jpg";
+// import ProductBest from "../../public/productbest.jpg";
+// import ProductCloseUp from "../../public/productclose.jpg";
+// import Products1 from "../../public/products1.jpg";
+// import FarShotBest from "../../public/farshotbest.jpg";
+// import CloseBest from "../../public/closebest.jpg";
+// import EdgeWork1 from "../../public/edgework1.jpg";
+// import FarShot1 from "../../public/farshot1.jpg";
+// import FightClimateChange from "../../public/Fight-climate-change-art.jpg";
+// import KeepTahoeBlue from "../../public/keeptahoeblue.png";
+// import Kirkwood from "../../public/logo-kirkwood.png";
+// import Mammoth from "../../public/mammothlogo.jpg";
+// import Pow from "../../public/POW.png";
+// import products1 from "../../public/products1.jpg";
+// import Purl from "../../public/purllogo.png";
+import HomeSplash from '../components/HomeSplash/HomeSplash.js';
+import Mission from '@/components/Mission/Mission.js';
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
     <div>
       <title>FullSendWaxing</title>
+      <Header />
       <main className="bg-slate-800">
-        <section className="min-h-screen bg-[url('../../public/farshotbest.jpg')] bg-cover bg-bottom">
-          <nav className="py-10 mb-12 flex justify-between text-white">
-            <h1 className="font-burtons text-xl px-10">Contact</h1>
-            <ul className="flex items-center">
-              <li className="px-10">
-                <BsFillMoonStarsFill
-                  onClick={() => setDarkMode(!darkMode)}
-                  className=" cursor-pointer text-2xl"
-                />
-              </li>
-              <li>
-                <a
-                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="#"
-                >
-                Shred Blog
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
-              <Image src={FswLogoWhite} 
-              layout="fill"
-              objectFit="cover"/>
-          </div>
-          <div className="text-center p-10 py-10">
-            <p className="text-md py-5 leading-8 text-white max-w-xl mx-auto md:text-xl">
-            </p>
-            <div className="text-5xl flex justify-center gap-16 py-3 text-white">
-              <AiFillInstagram />
-              <AiFillLinkedin />
-              <AiFillYoutube />
-            </div>
-          </div>
-        </section>
+        <HomeSplash darkMode={darkMode} />
+        <Mission />
         
       </main>
     </div>
--- src/FullSendWaxing.Web/full-send-waxing-web/src/components/Header/Header.js
+++ src/FullSendWaxing.Web/full-send-waxing-web/src/components/Header/Header.js
@@ -1,13 +1,37 @@
-import Link from 'next/link';
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
+import React, { useState } from 'react';
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import Logo from "../../../public/logodark.png";
+import Image from 'next/image';
 
+const Header = () => {
+  const [darkMode, setDarkMode] = useState(false);
 
-const Header = () =>  (
-  <div>
-    Header
-  </div>
-);
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  }
+
+  return (
+    <nav className="fixed top-0 w-full bg-slate-900 p-4 text-white flex justify-between items-center">
+      <div className="flex items-center">
+        <a href="#" onClick={scrollToTop}>
+          <Image src={Logo} alt="Logo" className="h-12 w-12 mr-4 rounded-full" />
+        </a>
+        <button className="bg-teal-500 hover:bg-teal-600 text-white py-2 px-6 rounded-full font-semibold text-lg mx-5">Shred Blog</button>
+      </div>
+      <div className="flex items-center">
+        <button className="bg-teal-500 hover:bg-teal-600 text-white py-2 px-6 rounded-full font-semibold text-lg mx-5">Contact</button>
+        <button
+          className="bg-teal-500 hover:bg-teal-600 text-white py-2 px-6 rounded-full font-semibold text-lg mx-5"
+          onClick={() => setDarkMode(!darkMode)}
+        >
+          <BsFillMoonStarsFill className="hover:text-slate-300" />
+        </button>
+      </div>
+    </nav>
+  );
+}
 
 export default Header;
